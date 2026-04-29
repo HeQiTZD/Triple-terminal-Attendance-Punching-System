733f19db89c20a023c76e8efad0b969517796333
HeQiTZD <sflores35megub@gmail.com>
2026-04-29 20:29:20 +0800

    1

--- out/build/debug/.qt/appAttendanceServer_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appAttendanceServer")
-set(working_dir "E:/project/AttendanceServer")
-set(src_and_dest_list
-    "E:/project/AttendanceServer/ui/Main.qml"
-    "E:/project/AttendanceServer/out/build/debug/AttendanceServer/ui/Main.qml"
-
-)
-set(timestamp_file "E:/project/AttendanceServer/out/build/debug/.qt/appAttendanceServer_qml.txt")
